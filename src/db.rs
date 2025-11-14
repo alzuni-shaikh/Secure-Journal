140cda74fcb74eb8318b4ab5c22b718d2024707e
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-14 16:59:17 +0530

    minor db issue causing runtime error fixed

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,12 @@
-use anyhow::Result;
 use surrealdb::Surreal;
 use surrealdb::engine::local::{Db, Mem};
+use anyhow::Result;
 
 pub async fn connect() -> Result<Surreal<Db>> {
     let db = Surreal::new::<Mem>(()).await?;
-    println!("Connected to in-memory SurrealDB (Mem engine).");
+
+    db.use_ns("app").use_db("journal").await?;
+
+    println!("Connected to in-memory SurrealDB");
     Ok(db)
 }
