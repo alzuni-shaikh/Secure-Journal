3138ad3c756790682a4f81834e9e5ff247e3dedc
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-23 15:14:50 +0530

    minor chnages to db

--- backend/src/bin/cli.rs
+++
@@ -1,6 +0,0 @@
-#[tokio::main]
-pub async fn main() -> anyhow::Result<()> {
-    let db = db::init_db().await?;
-    main_menu(&db).await?;
-    Ok(())
-}
--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,44 +1,9 @@
+use sqlx::{SqlitePool, Sqlite};
 use anyhow::Result;
-use sqlx::{Pool, Sqlite, SqlitePool};
 
-pub type DbPool = Pool<Sqlite>;
+pub type DbPool = SqlitePool;
 
-pub async fn init_db() -> Result<DbPool> {
-    std::fs::create_dir_all("data")?;
-    // let pool = SqlitePool::connect("data/journal.db").await?;\
-    // let pool = SqlitePool::connect("sqlite://journal.db").await?; //or
-    let pool = SqlitePool::connect("data/journal.db").await?;
-    
-    sqlx::query(
-        r#"
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password_hash TEXT NOT NULL
-        );
-
-        CREATE TABLE IF NOT EXISTS entries (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            content TEXT NOT NULL,
-            created_at TEXT NOT NULL,
-            FOREIGN KEY(user_id) REFERENCES users(id)
-        );
-        "#,
-    )
-    .execute(&pool)
-    .await?;
-
+pub async fn connect() -> Result<DbPool> {
+    let pool = SqlitePool::connect("sqlite://journal.db").await?;
     Ok(pool)
 }
-
-// pub async fn save_db(db: &Surreal<Db>) -> Result<()> {
-//     let export = db.export(()).await?;
-//     let _ = fs::write("db_backup.json", export)?;
-//     Ok(())
-// }
-// pub async fn load_users() -> Result<Vec<User>> {
-//     let data = fs::read_to_string("users.json").unwrap_or_else(|_| "[]".into());
-//     let users: Vec<User> = serde_json::from_str(&data)?;
-//     Ok(users)
-// }
