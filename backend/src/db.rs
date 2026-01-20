2d2fb1c0130bcf4c9c88524a16f5b975eec23759
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-01-20 18:06:11 +0530

    major database issue fixed

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,9 +1,16 @@
-use sqlx::SqlitePool;
 use anyhow::Result;
+use sqlx::SqlitePool;
+// use std::{env, path::PathBuf};
 
 pub type DbPool = SqlitePool;
 
 pub async fn connect() -> Result<DbPool> {
-    let pool = SqlitePool::connect("sqlite:///home/ibrahim/code/rust_programming/secure_journal/backend/data/journal.db").await?;
+    // let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    // path.push("data/journal.db");
+    // let url = format!("sqlite: {}", path.display());
+    // let pool = SqlitePool::connect(&url).await?;
+
+    let pool = SqlitePool::connect("sqlite:data/journal.db?mode=rwc").await?;
     Ok(pool)
 }
+
